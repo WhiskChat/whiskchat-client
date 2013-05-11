4c5689f23cac56fec45efa4462fa526a7d208f25
whiskers75 <whiskers75@me.com>
2013-05-11 12:46:16 +0100

    Report spam level

--- CoinChat_files/scripts.js
+++ CoinChat_files/scripts.js
@@ -613,7 +613,7 @@ socket.on("chat", function(data){
     if(data.user == username){
 	var m = "";
 	if (spammyness > 0) {
-            winBTCtext += " <span class='label label-info'>Spam level: " + Math.floor(spammyness) + "</span> ";
+            winBTCtext += " <span class='badge badge-info'>Spam level: " + Math.floor(spammyness) + "</span> ";
 	}
     } else {
 	var m = "";
