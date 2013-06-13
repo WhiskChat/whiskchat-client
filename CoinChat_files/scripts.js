448d5f9a625879e3411851c3003d518ce99f65a7
whiskers75 <whiskers75@me.com>
2013-06-13 17:28:41 +0100

    !; notice and latest Bootstrap

--- CoinChat_files/scripts.js
+++ CoinChat_files/scripts.js
@@ -695,6 +695,12 @@ socket.on("chat", function(data){
 	moveWin();
 	return;
     }
+    if (data.message.substr(0, 9) == '!; notice') {
+        $("#chattext").append("<div class='chatline'><span class='user' onclick='place()' style='background: rgba(136, 238, 136, 0.64);'><span></span>&nbsp;&nbsp;</span><span class='message muted' style='background: #eee'><strong>NOTICE</strong> " + data.message.substr(10, data.message.length) + "</span></div>");
+        addToRoomHTML("<div class='chatline'><span class='user' onclick='place()' style='background: rgba(136, 238, 136, 0.64);'><span></span>&nbsp;&nbsp;</span><span class='message muted' style='background: #eee'><strong>" + data.user + "</strong> connected to CoinChat (" + data.message.substr(11, data.message.length) + ")</span></div>");
+        moveWin();
+        return;
+    }
     if (data.message == '!; joinroom') {
         $("#chattext").append("<div class='chatline'><span class='user' onclick='place()' style='background: rgba(136, 238, 136, 0.64);'><span></span>&nbsp;&nbsp;</span><span class='message muted' style='background: #eee'><strong>" + data.user + "</strong> joined #" + data.room + "<span></div>");
         addToRoomHTML("<div class='chatline'><span class='user' onclick='place()' style='background: rgba(136, 238, 136, 0.64);'><span></span>&nbsp;&nbsp;</span><span class='message muted' style='background: #eee'><strong>" + data.user + "</strong> joined #" + data.room + "<span></div>");
