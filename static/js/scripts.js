fe45d347b0ef4074caad908fb7409dda56697777
whiskers75 <whiskers75@me.com>
2013-06-29 09:46:37 +0100

    Oops, be quiet

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -865,7 +865,7 @@ socket.on("chat", function(data){
 	if(!roomHTML[data.room]){
 	    roomHTML[data.room] = "";
 	}
-	if (data.user == "!Topic") {
+	if (data.user !== "!Topic") {
 	    roomHTML[data.room] += "<div class='chatline' title='" + data.timestamp + "'><span class='user' onclick='clickUser($(this).attr(\"data-user\"))' data-user='" + data.user + "'><span>" + data.user + "</span></span><span class='message" + m + "'>" + data.message + "<span class='foo'></span>" + winBTCtext + dateFormat + "</span></div>";
 	}
 	else {
